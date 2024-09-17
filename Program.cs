05f8247f01c7b50ad0a8432437935824bf43c94b
SkrillXXx <ebanutuijh@mail.ru>
2024-09-17 10:26:07 +0500

    check

--- Program.cs
+++ Program.cs
@@ -1,10 +1,27 @@
-﻿namespace fds
+﻿using System;
+namespace fds;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        // Ввод пользователя
+        Console.Write("Введите первый элемент (a1): ");
+        double a1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите разность прогрессии (d): ");
+        double d = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите количество элементов (n): ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        // Вычисление и вывод элементов прогрессии
+        Console.WriteLine("Элементы арифметической прогрессии:");
+
+        for (int i = 0; i < n; i++)
         {
-            Console.WriteLine("Hello, World!");
+            double an = a1 + i * d; // Формула для нахождения n-го элемента
+            Console.WriteLine($"a{i + 1} = {an}");
         }
     }
 }
